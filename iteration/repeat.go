a7003ce5b1cbe8b60a847257f8ee8923b5eeb2f6
Lucas Daniel <lucasdaniel.geo@gmail.com>
2023-08-29 20:05:22 -0300

    feat: add times to repeat

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, times int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < times; i++ {
 		repeated = repeated + character
 	}
 	return repeated
