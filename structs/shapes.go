101453ab79670363d3227561cb43dce9ce386053
Lucas Daniel <lucasdaniel.geo@gmail.com>
2023-08-30 18:02:08 -0300

    feat: add methods to circle and rectangle calculations

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,9 +1,24 @@
 package structs
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Rectangle struct {
+	Widht  float64
+	Height float64
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+func (r Rectangle) Area() float64 {
+	return r.Widht * r.Height
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Height + rectangle.Widht)
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
