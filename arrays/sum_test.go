b85623e945d0eb7f2565cab5791f7cc95af6876e
Lucas Daniel <lucasdaniel.geo@gmail.com>
2023-08-30 10:55:06 -0300

    feat: use slices and update tests

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,14 +3,14 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
 		got := Sum(numbers)
-		want := 15
+		want := 6
 
 		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
+			t.Errorf("got %d want %d, given %v", got, want, numbers)
 		}
 	})
 
