4f31e69ce8020b9e5eb9bac1ca5c322a7f9cfbe8
Lucas Daniel <lucasdaniel.geo@gmail.com>
2023-08-30 18:32:31 -0300

    feat: add table driven tests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,6 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+
 	checkArea := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
 
