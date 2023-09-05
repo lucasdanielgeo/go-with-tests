28e346c57b41da867de8e66fb3d73bc1cc5ac849
Lucas Daniel <lucasdaniel.geo@gmail.com>
2023-09-05 13:59:51 -0300

    refact: isolate helper for assertion of strings

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -8,7 +8,13 @@ func TestSearch(t *testing.T) {
 	got := Search(dictionary, "test")
 	want := "this is just a test"
 
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
 	if got != want {
-		t.Errorf("got %q want %q, given %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
 	}
 }
