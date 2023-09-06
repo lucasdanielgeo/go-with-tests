d09350652377a4b7aefb27770c76d7a494549d23
Lucas Daniel <lucasdaniel.geo@gmail.com>
2023-09-06 11:57:52 -0300

    feat: add incompatible type after dependency type definition

--- dependency-injection/di.go
+++ dependency-injection/di.go
@@ -3,8 +3,13 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"os"
 )
 
 func Greet(writer *bytes.Buffer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
 }
