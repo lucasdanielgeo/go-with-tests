b73f8a32742f11b7346dcc6857d56f291f93b2bf
Lucas Daniel <lucasdaniel.geo@gmail.com>
2023-09-06 10:46:18 -0300

    test: add tests for existing word

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -35,14 +35,29 @@ func assertError(t testing.TB, got, want error) {
 	}
 }
 
-func testAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
+func TestAdd(t *testing.T) {
 
-	dictionary.Add(word, definition)
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
 
-	assertDefinition(t, dictionary, word, definition)
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
