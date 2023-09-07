c5f5332cc8e8905322aea20a614e946c3d88016d
Lucas Daniel <lucasdaniel.geo@gmail.com>
2023-09-07 19:06:24 -0300

    tests: add a spy for all count down opearations

--- mocking/main.go
+++ mocking/main.go
@@ -16,14 +16,6 @@ type Sleeper interface {
 	Sleep()
 }
 
-type SpySleeper struct {
-	Calls int
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-
 type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() {
