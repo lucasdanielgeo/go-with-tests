02b12060531496bc698d70a945f8048940e9f7a4
Lucas Daniel <lucasdaniel.geo@gmail.com>
2023-09-09 19:18:42 -0300

    feat: add timeout to the race with time.After

--- select/racer.go
+++ select/racer.go
@@ -1,16 +1,21 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
-func Racer(a string, b string) (winner string) {
+func Racer(a string, b string) (winner string, error error) {
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("timed out wating for %s and %s", a, b)
 	}
+
 }
 
 func ping(url string) chan struct{} {
--- select/racer_test.go
+++ select/racer_test.go
@@ -8,24 +8,45 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeMDelayedServer(20 * time.Millisecond)
-	fastServer := makeMDelayedServer(0 * time.Millisecond)
+	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if err != nil {
+			t.Fatalf("did not expect an error but got one %v", err)
+		}
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns a error i a sever doesn't espond within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err != nil {
+			t.Error("expected an error but didn't get one")
+		}
+
+	})
 }
 
-func makeMDelayedServer(delay time.Duration) *httptest.Server {
+func makeDelayedServer(delay time.Duration) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(delay)
 		w.WriteHeader(http.StatusOK)
