b48f3ac4bbcbc6ffffcc0c1221bc1db3b84231eb
Lucas Daniel <lucasdaniel.geo@gmail.com>
2023-09-10 09:20:46 -0300

    feat: use reflection to assume values from an interface{}/any

---
+++ reflection/walk_test.go
@@ -0,0 +1,29 @@
+package walk
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	expected := "Chris"
+	var got []string
+
+	x := struct {
+		Name string
+	}{expected}
+
+	walk(x, func(input string) {
+		fmt.Print(&got)
+		got = append(got, input)
+		fmt.Print(&got)
+	})
+
+	if len(got) != 1 {
+		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	}
+
+	if got[0] != expected {
+		t.Errorf("got %q, want %q", got[0], expected)
+	}
+}
