de2b12c89ef753218977f505d5f66908f01614fa
Lucas Daniel <lucasdaniel.geo@gmail.com>
2023-09-20 18:21:00 -0300

    refact: add asserting functions with helpers

--- context/context_test.go
+++ context/context_test.go
@@ -11,6 +11,7 @@ import (
 type SpyStore struct {
 	response  string
 	cancelled bool
+	t         *testing.T
 }
 
 func (s *SpyStore) Fetch() string {
@@ -22,10 +23,41 @@ func (s *SpyStore) Cancel() {
 	s.cancelled = true
 }
 
+func (s *SpyStore) assertWasCancelled() {
+	s.t.Helper()
+	if !s.cancelled {
+		s.t.Error("store was not told to cancel")
+	}
+}
+
+func (s *SpyStore) assertWasNotCancelled() {
+	s.t.Helper()
+	if s.cancelled {
+		s.t.Error("store was told to cancel")
+	}
+}
+
 func TestServer(t *testing.T) {
+	data := "hello, world"
+
+	t.Run("returns data from store", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+		}
+
+		store.assertWasNotCancelled()
+	})
+
 	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
-		data := "hello, world"
-		store := &SpyStore{response: data}
+		store := &SpyStore{response: data, t: t}
 		svr := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -38,29 +70,6 @@ func TestServer(t *testing.T) {
 
 		svr.ServeHTTP(response, request)
 
-		if !store.cancelled {
-			t.Error("store was not told to cancel")
-		}
+		store.assertWasCancelled()
 	})
-
-	t.Run("returns data from store", func(t *testing.T) {
-		data := "hello, world"
-		store := &SpyStore{response: data}
-		svr := Server(store)
-
-		request := httptest.NewRequest(http.MethodGet, "/", nil)
-		response := httptest.NewRecorder()
-
-		svr.ServeHTTP(response, request)
-
-		if response.Body.String() != data {
-			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
-		}
-
-		if store.cancelled {
-			t.Error("it should not have cancelled the store")
-		}
-
-	})
-
 }
