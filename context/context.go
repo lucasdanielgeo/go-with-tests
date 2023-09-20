170a4948d731b62935f6783c744b1f2d491bed5f
Lucas Daniel <lucasdaniel.geo@gmail.com>
2023-09-20 18:10:54 -0300

    feat: add context to tell request is cancelled

--- context/context.go
+++ context/context.go
@@ -7,10 +7,26 @@ import (
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, store.Fetch())
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+
+		case <-ctx.Done():
+			store.Cancel()
+		}
+
 	}
 }
 
 type Store interface {
 	Fetch() string
+	Cancel()
 }
