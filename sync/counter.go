35c4dff861e5ccf0494172629e8b166b00a08807
Lucas Daniel <lucasdaniel.geo@gmail.com>
2023-09-20 11:08:24 -0300

    fix: use mutex to fix concurrent write on counter

--- sync/counter.go
+++ sync/counter.go
@@ -1,10 +1,17 @@
 package counter
 
+import "sync"
+
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
-func (c *Counter) Inc() { c.value++ }
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	c.value++
+	c.mu.Unlock()
+}
 func (c *Counter) Value() int {
 	return c.value
 }
