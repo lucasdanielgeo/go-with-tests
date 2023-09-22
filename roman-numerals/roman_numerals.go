d775af6eeee3ea550247c4ebbd977f0d5167b07c
Lucas Daniel <lucasdaniel.geo@gmail.com>
2023-09-21 23:56:49 -0300

    refact: decoupling dataa from behaviour

--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -2,29 +2,28 @@ package romannumerals
 
 import "strings"
 
-// var numbersMap map[int16]string
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
 
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
-	for arabic > 0 {
-		switch {
-		case arabic > 9:
-			result.WriteString("X")
-			arabic -= 10
-
-		case arabic > 8:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic > 4:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic -= 1
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
 	}
 
